fd5aba8b7fa309b51f563bc79b882118d735c4cf
Jan Jakubowski <jan.jakubowski.us@ieee.org>
2019-07-11 11:50:21 -0400

    sequelize the initialization and comment out all the routing in controller.js

--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,16 @@
-var orm = require("../config/orm.js");
+// module.exports = function(sequelize, DataTypes) {
+// 	var Burger = sequelize.define("Burger", {
+// 	  burger_name: DataTypes.STRING,
+// 	  devoured: DataTypes.BOOLEAN
+// 	});
+// 	return Burger;
+//   };
+module.exports = function(sequelize, DataTypes) {
+	var Burger = sequelize.define("Burger", {
+	  burger_name: DataTypes.STRING,
+	  devoured: DataTypes.BOOLEAN
+	});
+	return Burger;
+  };
 
-var burger = {
-	all: function(callback) {
-		orm.selectAll("burgers", function(res) {
-			callback(res);
-		});
-	},
-	// The variables cols and vals are arrays.
-	create: function(cols, vals, callback) {
-		orm.insertOne("burgers", cols, vals, function(res) {
-			callback(res);
-		});
-	},
-	update: function(objColVals, condition, callback) {
-		orm.update("burgers", objColVals, condition, function(res) {
-			callback(res);
-		});
-	},
-	delete: function(condition, callback) {
-		orm.delete("burgers", condition, function(res) {
-			callback(res);
-		});
-	}
-};
-
-// Export the database functions for the controller (burgers_controller.js).
-module.exports = burger;
   
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // Setup express 
 var express = require("express");
-var PORT = process.env.PORT || 8080;
 var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -19,8 +22,17 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: " + PORT);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
+
+// Start our server so that it can begin listening to client requests.
+// app.listen(PORT, function() {
+//   // Log (server-side) when our server has started
+//   console.log("Server listening on: " + PORT);
+// });
+
